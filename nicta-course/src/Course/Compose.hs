396a037a0ce9506a85b7da1e3057930106cf22fb
Oleh Stolyar <stolyar.oleh@gmail.com>
2016-04-06 02:05:26 +0300

    NICTA - Compose

--- nicta-course/src/Course/Compose.hs
+++ nicta-course/src/Course/Compose.hs
@@ -16,20 +16,23 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  f <$> (Compose fg) =
+    Compose $ (f <$>) <$> fg
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+  pure = Compose . pure . pure
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  -- f :: f (g (a -> b))
+  -- f' :: f (g a -> g b)
+  (Compose f) <*> (Compose fg) =
+    Compose $ f' <*> fg
+    where f' = (<*>) <$> f
+    -- or lift2 (<*>) f fg
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
   (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+    error "Impossible"
